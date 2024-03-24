c025b1b18485d83bef14ec833bdfe970e8b18934
Aman Sinha <amansinha561@gmail.com>
2024-03-24 13:27:22 +0530

    added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import React from 'react';
-import Navbar from './components/Login_Customer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Login_Customer/>
+      <Navbar/>
     </div>
   );
 }
